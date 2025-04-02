ecb0dc0553c6ec8820b29d4e717ee351e00324d7
Jatan-vd <jatan.vd@gmail.com>
2025-04-02 14:22:41 +0530

    Add files via upload
    first code

---
+++ NoobHeadquarters_problem1_first.py
@@ -0,0 +1,25 @@
+m = int(input("Enter m "))
+n = int(input("Enter n "))
+a = [][]
+count = 1
+for i in range(n):
+    for i in range(m):
+        a.append(input("Enter grids"))
+    
+for i in range(1,m):
+    for j in range(n):
+        
+        if a[i][j] == 1:
+            if i == m-1:
+                if a[i-1][j] == 1:
+                    break
+                else:
+                    count += 1
+            if a[i-1][j] == 1 or a[i+1][j] == 1:
+                break
+            else:
+                count += 1
+                
+print(count)
+        
+
